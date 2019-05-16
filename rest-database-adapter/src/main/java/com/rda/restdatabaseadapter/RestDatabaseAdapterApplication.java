8d3e1ac0ac28b43d884152feda8733da6367516f
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2019-05-16 23:46:20 +0530

    [MAY|2019] #1 spring project initialized

---
+++ rest-database-adapter/src/main/java/com/rda/restdatabaseadapter/RestDatabaseAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.rda.restdatabaseadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDatabaseAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDatabaseAdapterApplication.class, args);
+	}
+
+}
---
+++ rest-database-adapter/src/test/java/com/rda/restdatabaseadapter/RestDatabaseAdapterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rda.restdatabaseadapter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestDatabaseAdapterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
